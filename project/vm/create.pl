002198220c7fc741578301e9c51b50bfe284cfa2
jpicado <a@b>
2010-09-10 01:18:29 +0200

    Updating ESX plugin
    [git-p4: depot-paths = "//depot/nimbus/integrations/EC-ESX/main/": change = 46199]

--- project/vm/create.pl
+++ project/vm/create.pl
@@ -12,12 +12,18 @@ $opts->{esx_datastore} = "$[esx_datastore]";
 $opts->{esx_vmhost} = "$[esx_vmhost]";
 $opts->{esx_datacenter} = "$[esx_datacenter]";
 $opts->{esx_guestid} = "$[esx_guestid]";
-$opts->{esx_disksize} = $[esx_disksize];
-$opts->{esx_memory} = $[esx_memory];
-$opts->{esx_num_cpus} = $[esx_num_cpus];
+if($opts->{esx_guestid} eq "") { $opts->{esx_guestid} = "winXPProGuest" }
+$opts->{esx_disksize} = "$[esx_disksize]";
+if($opts->{esx_disksize} eq "") { $opts->{esx_disksize} = 4096; }
+$opts->{esx_memory} = "$[esx_memory]";
+if($opts->{esx_memory} eq "") { $opts->{esx_memory} = 256; }
+$opts->{esx_num_cpus} = "$[esx_num_cpus]";
+if($opts->{esx_num_cpus} eq "") { $opts->{esx_num_cpus} = 1; }
 $opts->{esx_nic_network} = "$[esx_nic_network]";
-$opts->{esx_nic_poweron} = $[esx_nic_poweron];
-$opts->{esx_number_of_vms} = $[esx_number_of_vms];
+$opts->{esx_nic_poweron} = "$[esx_nic_poweron]";
+if($opts->{esx_nic_poweron} eq "") { $opts->{esx_nic_poweron} = 1; }
+$opts->{esx_number_of_vms} = "$[esx_number_of_vms]";
+if($opts->{esx_number_of_vms} eq "") { $opts->{esx_number_of_vms} = 1; }
 $opts->{exitcode} = 0;
 
 $[/myProject/procedure_helpers/preamble]
