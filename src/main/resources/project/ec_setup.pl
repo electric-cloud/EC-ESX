4d32537b44993796ee5c101245c0ef55c279cd4e
rsanchez <a@b>
2012-04-20 05:38:49 +0200

    ESX procedure name(s) were changed in the step picker section
    [git-p4: depot-paths = "//depot/nimbus/integrations/EC-ESX/main/": change = 59652]

--- src/main/resources/project/ec_setup.pl
+++ src/main/resources/project/ec_setup.pl
@@ -1,5 +1,5 @@
 my %suspend = (
-			   label       => "ESX - Suspend",
+			   label       => "ESX - Suspend Virtual Machine",
 			   procedure   => "Suspend",
 			   description => "Suspend a virtual machine",
 			   category    => "Resource Management"
@@ -121,8 +121,275 @@ $batch->deleteProperty("/server/ec_customEditors/pickerStep/EC-ESX - Snapshot");
 $batch->deleteProperty("/server/ec_customEditors/pickerStep/EC-ESX - CloudManagerShrink");
 $batch->deleteProperty("/server/ec_customEditors/pickerStep/EC-ESX - CloudManagerGrow");
 
+$batch->deleteProperty("/server/ec_customEditors/pickerStep/ESX - Suspend Virtual Machine");
+$batch->deleteProperty("/server/ec_customEditors/pickerStep/ESX - Suspend");
+$batch->deleteProperty("/server/ec_customEditors/pickerStep/ESX - Clone Virtual Machine");
+$batch->deleteProperty("/server/ec_customEditors/pickerStep/ESX - Cleanup Virtual Machine");
+$batch->deleteProperty("/server/ec_customEditors/pickerStep/ESX - Create Virtual Machine");
+$batch->deleteProperty("/server/ec_customEditors/pickerStep/ESX - Create Resource From Virtual Machine");
+$batch->deleteProperty("/server/ec_customEditors/pickerStep/ESX - Import");
+$batch->deleteProperty("/server/ec_customEditors/pickerStep/ESX - Get Virtual Machine Configuration");
+$batch->deleteProperty("/server/ec_customEditors/pickerStep/ESX - Export");
+$batch->deleteProperty("/server/ec_customEditors/pickerStep/ESX - Relocate Virtual Machine");
+$batch->deleteProperty("/server/ec_customEditors/pickerStep/ESX - Register Virtual Machine");
+$batch->deleteProperty("/server/ec_customEditors/pickerStep/ESX - Power Off Virtual Machine");
+$batch->deleteProperty("/server/ec_customEditors/pickerStep/ESX - Power On Virtual Machine");
+$batch->deleteProperty("/server/ec_customEditors/pickerStep/ESX - Revert");
+$batch->deleteProperty("/server/ec_customEditors/pickerStep/ESX - Shutdown Virtual Machine");
+$batch->deleteProperty("/server/ec_customEditors/pickerStep/ESX - Snapshot");
+
 @::createStepPickerSteps = (\%suspend, \%cleanup, \%clone, \%create, \%createresourcefromvm, \%import, \%getvmconfiguration, \%export, \%relocate, \%registervm, \%poweroff, \%poweron, \%revert, \%shutdown, \%snapshot);
 
+my $pluginName = "@PLUGIN_NAME@";
+my $pluginKey = "@PLUGIN_KEY@";
+if ($promoteAction ne '') {
+    my @objTypes = ('projects', 'resources', 'workspaces');
+    my $query    = $commander->newBatch();
+    my @reqs     = map { $query->getAclEntry('user', "project: $pluginName", { systemObjectName => $_ }) } @objTypes;
+    push @reqs, $query->getProperty('/server/ec_hooks/promote');
+    $query->submit();
+
+    foreach my $type (@objTypes) {
+        if ($query->findvalue(shift @reqs, 'code') ne 'NoSuchAclEntry') {
+            $batch->deleteAclEntry('user', "project: $pluginName", { systemObjectName => $type });
+        }
+    }
+
+    if ($promoteAction eq "promote") {
+        foreach my $type (@objTypes) {
+            $batch->createAclEntry(
+                                   'user',
+                                   "project: $pluginName",
+                                   {
+                                      systemObjectName           => $type,
+                                      readPrivilege              => 'allow',
+                                      modifyPrivilege            => 'allow',
+                                      executePrivilege           => 'allow',
+                                      changePermissionsPrivilege => 'allow'
+                                   }
+                                  );
+        }
+    }
+}
+
+if ($upgradeAction eq "upgrade") {
+    my $query   = $commander->newBatch();
+    my $newcfg  = $query->getProperty("/plugins/$pluginName/project/esx_cfgs");
+    my $oldcfgs = $query->getProperty("/plugins/$otherPluginName/project/esx_cfgs");
+    my $creds   = $query->getCredentials("\$[/plugins/$otherPluginName]");
+
+    local $self->{abortOnError} = 0;
+    $query->submit();
+
+    # if new plugin does not already have cfgs
+    if ($query->findvalue($newcfg, "code") eq "NoSuchProperty") {
+
+        # if old cfg has some cfgs to copy
+        if ($query->findvalue($oldcfgs, "code") ne "NoSuchProperty") {
+            $batch->clone(
+                          {
+                            path      => "/plugins/$otherPluginName/project/esx_cfgs",
+                            cloneName => "/plugins/$pluginName/project/esx_cfgs"
+                          }
+                         );
+        }
+    }
+
+    # Copy configuration credentials and attach them to the appropriate steps
+    my $nodes = $query->find($creds);
+    if ($nodes) {
+        my @nodes = $query->{xpath}->findnodes("credential/credentialName", $nodes);
+        for (@nodes) {
+            my $cred = $_->string_value;
+
+            # Clone the credential
+            $batch->clone(
+                          {
+                            path      => "/plugins/$otherPluginName/project/credentials/$cred",
+                            cloneName => "/plugins/$pluginName/project/credentials/$cred"
+                          }
+                         );
+
+            # Make sure the credential has an ACL entry for the new project principal
+            my $xpath = $commander->getAclEntry(
+                                                "user",
+                                                "project: $pluginName",
+                                                {
+                                                   projectName    => $otherPluginName,
+                                                   credentialName => $cred
+                                                }
+                                               );
+            if ($xpath->findvalue("//code") eq "NoSuchAclEntry") {
+                $batch->deleteAclEntry(
+                                       "user",
+                                       "project: $otherPluginName",
+                                       {
+                                          projectName    => $pluginName,
+                                          credentialName => $cred
+                                       }
+                                      );
+                $batch->createAclEntry(
+                                       "user",
+                                       "project: $pluginName",
+                                       {
+                                          projectName                => $pluginName,
+                                          credentialName             => $cred,
+                                          readPrivilege              => 'allow',
+                                          modifyPrivilege            => 'allow',
+                                          executePrivilege           => 'allow',
+                                          changePermissionsPrivilege => 'allow'
+                                       }
+                                      );
+            }
+
+            # Attach the credential to the appropriate steps
+            $batch->attachCredential(
+                                     "\$[/plugins/$pluginName/project]",
+                                     $cred,
+                                     {
+                                        procedureName => 'Create',
+                                        stepName      => 'Create'
+                                     }
+                                    );
+            $batch->attachCredential(
+                                     "\$[/plugins/$pluginName/project]",
+                                     $cred,
+                                     {
+                                        procedureName => 'Clone',
+                                        stepName      => 'Clone'
+                                     }
+                                    );
+            $batch->attachCredential(
+                                     "\$[/plugins/$pluginName/project]",
+                                     $cred,
+                                     {
+                                        procedureName => 'Relocate',
+                                        stepName      => 'Relocate'
+                                     }
+                                    );
+            $batch->attachCredential(
+                                     "\$[/plugins/$pluginName/project]",
+                                     $cred,
+                                     {
+                                        procedureName => 'Cleanup',
+                                        stepName      => 'Cleanup'
+                                     }
+                                    );
+            $batch->attachCredential(
+                                     "\$[/plugins/$pluginName/project]",
+                                     $cred,
+                                     {
+                                        procedureName => 'Revert',
+                                        stepName      => 'Revert'
+                                     }
+                                    );
+            $batch->attachCredential(
+                                     "\$[/plugins/$pluginName/project]",
+                                     $cred,
+                                     {
+                                        procedureName => 'Snapshot',
+                                        stepName      => 'Snapshot'
+                                     }
+                                    );
+            $batch->attachCredential(
+                                     "\$[/plugins/$pluginName/project]",
+                                     $cred,
+                                     {
+                                        procedureName => 'PowerOn',
+                                        stepName      => 'PowerOn'
+                                     }
+                                    );
+            $batch->attachCredential(
+                                     "\$[/plugins/$pluginName/project]",
+                                     $cred,
+                                     {
+                                        procedureName => 'PowerOff',
+                                        stepName      => 'PowerOff'
+                                     }
+                                    );
+            $batch->attachCredential(
+                                     "\$[/plugins/$pluginName/project]",
+                                     $cred,
+                                     {
+                                        procedureName => 'Shutdown',
+                                        stepName      => 'Shutdown'
+                                     }
+                                    );
+            $batch->attachCredential(
+                                     "\$[/plugins/$pluginName/project]",
+                                     $cred,
+                                     {
+                                        procedureName => 'Suspend',
+                                        stepName      => 'Suspend'
+                                     }
+                                    );
+            $batch->attachCredential(
+                                     "\$[/plugins/$pluginName/project]",
+                                     $cred,
+                                     {
+                                        procedureName => 'CreateResourceFromVM',
+                                        stepName      => 'CreateResourceFromVM'
+                                     }
+                                    );
+            $batch->attachCredential(
+                                     "\$[/plugins/$pluginName/project]",
+                                     $cred,
+                                     {
+                                        procedureName => 'GetVMConfiguration',
+                                        stepName      => 'GetVMConfiguration'
+                                     }
+                                    );
+            $batch->attachCredential(
+                                     "\$[/plugins/$pluginName/project]",
+                                     $cred,
+                                     {
+                                        procedureName => 'Import',
+                                        stepName      => 'Import'
+                                     }
+                                    );
+            $batch->attachCredential(
+                                     "\$[/plugins/$pluginName/project]",
+                                     $cred,
+                                     {
+                                        procedureName => 'Export',
+                                        stepName      => 'Export'
+                                     }
+                                    );
+            $batch->attachCredential(
+                                     "\$[/plugins/$pluginName/project]",
+                                     $cred,
+                                     {
+                                        procedureName => 'RegisterVM',
+                                        stepName      => 'RegisterVM'
+                                     }
+                                    );
+            $batch->attachCredential(
+                                     "\$[/plugins/$pluginName/project]",
+                                     $cred,
+                                     {
+                                        procedureName => 'CloudManagerGrow',
+                                        stepName      => 'grow'
+                                     }
+                                    );
+            $batch->attachCredential(
+                                     "\$[/plugins/$pluginName/project]",
+                                     $cred,
+                                     {
+                                        procedureName => 'CloudManagerShrink',
+                                        stepName      => 'shrink'
+                                     }
+                                    );
+            $batch->attachCredential(
+                                     "\$[/plugins/$pluginName/project]",
+                                     $cred,
+                                     {
+                                        procedureName => 'CloudManagerSync',
+                                        stepName      => 'sync'
+                                     }
+                                    );
+        }
+    }
+}
     
 	
 	
