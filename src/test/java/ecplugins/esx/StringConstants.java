367f1a9755afa6819f01e57c182b45fa5f8158b7
Chani Jindal <chani.jindal@clogeny.com>
2016-05-12 19:16:11 +0530

    Added Unit Test for EC-ESX plugin

---
+++ src/test/java/ecplugins/esx/StringConstants.java
@@ -0,0 +1,34 @@
+/*
+ *
+ * Copyright 2015 Electric Cloud, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package test.java.ecplugins.esx;
+
+public class StringConstants {
+
+	final static String COMMANDER_SERVER = "COMMANDER_SERVER";
+	final static String EC_AGENT_IP = "EC_AGENT_IP";
+	final static String EC_AGENT_PORT = "7800";
+	final static String RESOURCE_NAME = "local";
+	final static String WORKSPACE_NAME = "default";
+	final static String PLUGIN_VERSION = "2.2.0";
+	final static String COMMANDER_USER = "COMMANDER_USER";
+	final static String COMMANDER_PASSWORD = "COMMANDER_PASSWORD";
+	final static String SERVICE_URL = "SERVICE_URL";
+	final static String ACCESS_ID = "ACCESS_ID";
+	final static String SECRET_ACCESS_ID = "SECRET_ACCESS_ID";
+	final static long jobTimeoutMillis = 5 * 60 * 1000;
+}
